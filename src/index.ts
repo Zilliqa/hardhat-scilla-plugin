813482d014b848721869eb371fc02128b3b1ce2d
S4eed <saeed@zilliqa.com>
2023-04-13 18:00:59 +0330

    Add deployScillaFile function (#30)
    * Add deployByFile fucntion
    
    * Run linter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import { Transaction } from "@zilliqa-js/account";
+import { Init } from "@zilliqa-js/contract";
 import { extendEnvironment } from "hardhat/config";
 import { lazyFunction, lazyObject } from "hardhat/plugins";
 
 import {
   deploy,
+  deployFromFile,
   deployLibrary,
   ScillaContract,
   UserDefinedLibrary,
@@ -19,6 +22,7 @@ export {
   Setup,
   initZilliqa,
   setAccount,
+  UserDefinedLibrary,
 } from "./ScillaContractDeployer";
 
 export { scillaChaiEventMatcher } from "./ScillaChaiMatchers";
@@ -29,7 +33,7 @@ extendEnvironment((hre) => {
   // needed.
   hre.scillaContracts = lazyObject(() => loadScillaContractsInfo());
 
-  hre.deployScilla = lazyFunction(
+  hre.deployScillaContract = lazyFunction(
     () => async (
       contractName: string,
       ...args: any[]
@@ -38,7 +42,7 @@ extendEnvironment((hre) => {
     }
   );
 
-  hre.deployScillaWithLib = lazyFunction(
+  hre.deployScillaContractWithLib = lazyFunction(
     () => async (
       contractName: string,
       userDefinedLibraries: UserDefinedLibrary[],
@@ -51,6 +55,15 @@ extendEnvironment((hre) => {
   hre.deployScillaLibrary = lazyFunction(
     () => async (libraryName: string): Promise<ScillaContract> => {
       return deployLibrary(hre, libraryName);
+    }
+  );
+
+  hre.deployScillaFile = lazyFunction(
+    () => async (
+      contractPath: string,
+      init: Init
+    ): Promise<[Transaction, ScillaContract]> => {
+      return deployFromFile(contractPath, init);
     }
   );
 
--- src/type-extensions.ts
+++ src/type-extensions.ts
@@ -1,6 +1,8 @@
 // If your plugin extends types from another plugin, you should import the plugin here.
 
 // To extend one of Hardhat's types, you need to import the module where it has been defined, and redeclare it.
+import { Transaction } from "@zilliqa-js/account";
+import { Init } from "@zilliqa-js/contract";
 import "hardhat/types/config";
 import "hardhat/types/runtime";
 
@@ -12,16 +14,20 @@ import { ZilliqaHardhatObject } from "./ZilliqaHardhatObject";
 declare module "hardhat/types/runtime" {
   export interface HardhatRuntimeEnvironment {
     scillaContracts: ScillaContracts;
-    deployScilla: (
+    deployScillaContract: (
       contractName: string,
       ...args: any[]
     ) => Promise<ScillaContract>;
-    deployScillaWithLib: (
+    deployScillaContractWithLib: (
       contractName: string,
       userDefinedLibraries: UserDefinedLibrary[],
       ...args: any[]
     ) => Promise<ScillaContract>;
     deployScillaLibrary: (contractName: string) => Promise<ScillaContract>;
+    deployScillaFile: (
+      contractName: string,
+      init: Init
+    ) => Promise<[Transaction, ScillaContract]>;
     zilliqa: ZilliqaHardhatObject;
   }
 }
